04ccf80ac8cc77f35d1cf9d3710a314777505df6
Martynas Kanapinskas <martis347@gmail.com>
2016-02-28 16:43:16 +0200

    Added icons of resize and 'take' for every card and binded it with mouseover event

--- init.js
+++ init.js
@@ -12,12 +12,26 @@ function init() {
 		resolution: window.devicePixelRatio,
 		autoResize: false,
 	}
-	var renderer = PIXI.autoDetectRenderer(gameWidth, gameHeigth, {view:document.getElementById("game-canvas")});
+	var renderer = PIXI.autoDetectRenderer(gameWidth, gameHeigth, {view:document.getElementById("game-canvas")}, true);
 	var stage = new PIXI.Container(0x66FF99);	
 	stage.scale.x = scaleRatio;
 	stage.scale.y = scaleRatio;
+	stage.interactive = true;
+
 
 	loadResources(stage, renderer);
+
+	var cont = new PIXI.Container();
+	var cardTexture = PIXI.Texture.fromImage("Resources/workers/" + "J1" + ".jpg");
+	var test = new PIXI.Sprite(cardTexture);
+	test.interactive = true;
+	test.hitArea =  new PIXI.Rectangle(0,0, 200, 200);
+	test.click = function(ev) { console.log("clicked"); }
+	test.mouseover = function(ev) { console.log("over"); }
+	test.mouseout = function(ev) { console.log("out"); }
+	test.position.x = 50;
+	test.position.y = 50;
+	cont.addChild(test);
 
 	requestAnimationFrame(animate);
 	function animate() {
@@ -55,7 +69,7 @@ function createWorkersContainer(){
 	var cards = randomCardsList(6);
 
 	for (var i = 0; i < cards.length; i++) {
-		var cardTexture = PIXI.Texture.fromImage("Resources/workers/" + cards[i] + ".jpg");
+		var cardTexture = new PIXI.Texture.fromImage("Resources/workers/" + cards[i] + ".jpg");
 		var cardSprite = new PIXI.Sprite(cardTexture);
 
 		if(i != cards.length - 1)
@@ -63,9 +77,34 @@ function createWorkersContainer(){
 			cardSprite.buttonMode = true;
 			cardSprite.interactive = true;
 			cardSprite.position.x = i * 110;
+			cardSprite.hitArea = new PIXI.Rectangle(0, 0, 107, 150);
 
-			cardSprite.mouseup = function(data){
-				console.log(data);
+			var takeSprite = new PIXI.Sprite();
+			takeSprite = takeIcon();
+			takeSprite.position.x = 70;
+
+			cardSprite.addChild(takeSprite);
+
+			var resizeSprite = new PIXI.Sprite();
+			resizeSprite = resizeIcon();
+			resizeSprite.position.x = 10;
+			
+			cardSprite.addChild(resizeSprite);
+
+			cardSprite.click = cardSprite.touchstart = function(ev){
+				console.log("click");
+			}
+
+			cardSprite.mouseover = function(ev){
+				this.children.forEach(function(element) {
+					element.visible = true;
+				});
+			}
+
+			cardSprite.mouseout = function(ev) {
+				this.children.forEach(function(element) {
+					element.visible = false;
+				});
 			}
 		}
 		else{
@@ -87,6 +126,18 @@ function createBuildingsContainer(){
 	for (var i = 0; i < cards.length; i++) {
 		var cardTexture = PIXI.Texture.fromImage("Resources/buildings/" + cards[i] + ".jpg");
 		var cardSprite = new PIXI.Sprite(cardTexture);
+		cardSprite.hitArea = new PIXI.Rectangle(0, 0, 150, 150);
+
+		var takeSprite = new PIXI.Sprite();
+		takeSprite = takeIcon();
+		takeSprite.position.x = 25;
+
+		cardSprite.addChild(takeSprite);
+
+		var resizeSprite = new PIXI.Sprite();
+		resizeSprite = resizeIcon();
+		resizeSprite.position.x = 100;
+		cardSprite.addChild(resizeSprite);
 
 		if(i != cards.length - 1)
 		{
@@ -94,8 +145,24 @@ function createBuildingsContainer(){
 			cardSprite.interactive = true;
 			cardSprite.position.x = i * 153;
 
-			cardSprite.mouseup = function(data){
+			cardSprite.click = function(data){
 				console.log(data);
+			}
+
+			cardSprite.click = cardSprite.touchstart = function(ev){
+				console.log("click");
+			}
+
+			cardSprite.mouseover = function(ev){
+				this.children.forEach(function(element) {
+					element.visible = true;
+				});
+			}
+
+			cardSprite.mouseout = function(ev) {
+				this.children.forEach(function(element) {
+					element.visible = false;
+				});
 			}
 		}
 		else{
@@ -106,6 +173,42 @@ function createBuildingsContainer(){
 	}
 
 	return buildingsContainer;
+}
+
+function takeIcon(action){
+	var take = PIXI.Texture.fromImage("Resources/takeS.png");
+	var takeSprite = new PIXI.Sprite(take);
+
+	takeSprite.position.y = 125;
+
+	takeSprite.interactive = true;
+	takeSprite.visible = false;
+
+	takeSprite.mouseup = action;
+
+	return takeSprite;
+}
+
+function resizeIcon(action){
+	var resize = new PIXI.Texture.fromImage("Resources/resizeS.png");
+	var resizeSprite = new PIXI.Sprite(resize);
+
+	resizeSprite.position.y = 125;
+
+	resizeSprite.interactive = true;
+	resizeSprite.visible = false;
+
+	resizeSprite.click = action;
+	
+	return resizeSprite;
+}
+
+function takeWorker(){
+	console.log("You have taken worker!");
+}
+
+function takeBuilding(){
+	console.log("You have taken building!");
 }
 
 function randomCardsList(cardsCount){
