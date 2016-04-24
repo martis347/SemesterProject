dbf2c20b488393df5ed06e5c37dd4c2537bad10a
Martynas Kanapinskas <martis347@gmail.com>
2016-04-24 21:54:38 +0300

    Made workers draggable

--- scripts/app/elements/cards/workerCard.js
+++ scripts/app/elements/cards/workerCard.js
@@ -7,16 +7,47 @@ define(['pixi', 'utils/mouseOver','utils/buttons'], function (PIXI, mouseOvers, 
             cardSprite = new PIXI.Sprite(cardTexture);
 
             cardSprite.interactive = true;
-            cardSprite.hitArea = new PIXI.Rectangle(0, 0, 107, 150);
             cardSprite.card = {};
             cardSprite.card.id = id;
             cardSprite.card.type = "worker";
             cardSprite.card.placement = "deck";
 
-            buttonsElement.add(buttonsPlacementType, cardSprite);
+            if(buttonsPlacementType !== "hand")
+            {
+                buttonsElement.add(buttonsPlacementType, cardSprite);
 
-            mouseOvers.add(cardSprite);
-
+                cardSprite.dragging
+                mouseOvers.add(cardSprite);
+            }
+            else {
+                cardSprite.buttonMode = true;
+                cardSprite.mousedown = cardSprite.touchstart = function(data)
+                {
+                    this.data = data;
+                    this.alpha = 0.9;
+                    this.dragging = true;
+                    this.sx = this.data.data.getLocalPosition(this).x * cardSprite.scale.x;
+                    this.sy = this.data.data.getLocalPosition(this).y * cardSprite.scale.x;		
+                };
+                
+                cardSprite.mouseup = cardSprite.mouseupoutside = cardSprite.touchend = cardSprite.touchendoutside = function(data)
+                {
+                    this.alpha = 1
+                    this.dragging = false;
+                    this.data = null;
+                };
+                
+                cardSprite.mousemove = cardSprite.touchmove = function(data)
+                {
+                    if(this.dragging)
+                    {
+                        var newPosition = this.data.data.getLocalPosition(this.parent);
+                        this.position.x = newPosition.x - this.sx;
+                        this.position.y = newPosition.y - this.sy;
+                    }
+                }
+            }
+            
             return cardSprite;
         },
         changeTexture: function (card, newId) {
