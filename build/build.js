81f696f8e2a19a96a16daf315239d7fde9c130ad
Martynas Kanapinskas <martis347@gmail.com>
2016-05-18 23:45:35 +0300

    Fixes for file optimisation

--- build/build.js
+++ build/build.js
@@ -8,7 +8,8 @@
         cards: 'elements/cards',
         utils: 'elements/utils',
         api: 'api',
-        jquery: 'https://code.jquery.com/jquery-2.2.3.min'
+        jquery: 'https://code.jquery.com/jquery-2.2.3.min',
+		sweetAlert: '../lib/sweetalert.min'
     },
     name: "../main",
     out: "../main-built.js"
